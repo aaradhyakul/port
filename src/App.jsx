fd736e18a87a876d2c658b96fa4f778fde205d9a
aaradhyakul <aaradhyakul@gmail.com>
2023-06-06 21:53:17 +0530

    mouse trail

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,76 @@
 import { BrowserRouter } from "react-router-dom"
 import {About,Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas} from "./components"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const App =()=> {
   const [active,setActive] = useState("")
   const activeHandler =(activeId)=>{
     setActive(activeId);
   }
+  useEffect(()=>{
+    const coords = {x:0,y:0};
+    const circles = document.querySelectorAll('.trail-scroll')
+    circles.forEach(circle=>{
+      circle.x = 0;
+      circle.y = 0;
+      
+
+    });
+
+    const animateCircles = () =>{
+      let x = coords.x;
+      let y = coords.y;
+
+      circles.forEach((circle,index)=>{
+        circle.style.left = x - 8 + "px";
+        circle.style.top = y - 8 + "px";
+        // circle.style.scale = (circles.length - index*0.4)/circles.length;
+        circle.style.opacity = (circles.length - index*0.9)/circles.length;
+        circle.style.zIndex = 10 + circles.length - index;
+        circle.x = x;
+        circle.y = y;
+        
+
+        const nextCircle = circles[index + 1] || circles[0]
+        x += (nextCircle.x - x)*0.7;
+        y += (nextCircle.y - y)*0.7;
+        
+
+      });
+    }
+
+    window.addEventListener('mousemove',(e)=>{
+      coords.x = e.clientX;
+      coords.y = e.clientY;
+      // console.log(coords)
+      animateCircles();
+     
+    })
+    setInterval(animateCircles,1000/60);
+
+
+
+  },[])
   return (
+    <>
+    <div className="trail-scroll leader"/>
+    <div className="trail-scroll followers"/>
+    <div className="trail-scroll followers"/>
+    <div className="trail-scroll followers"/>
+    <div className="trail-scroll followers"/>
+    <div className="trail-scroll followers"/>
+    <div className="trail-scroll followers"/>
+    <div className="trail-scroll followers"/>
+    <div className="trail-scroll followers"/>
+    <div className="trail-scroll followers"/>
+    <div className="trail-scroll followers"/>
+    <div className="trail-scroll followers"/>
+    <div className="trail-scroll followers"/>
+    <div className="trail-scroll followers"/>
+    <div className="trail-scroll followers"/>
+    <div className="trail-scroll followers"/>
+    
+    
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
           <Navbar activeHandler={activeHandler} active={active}/>
@@ -25,6 +88,7 @@ const App =()=> {
         </div>
       </div>
     </BrowserRouter>
+    </>
   )
 }
 
